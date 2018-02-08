c2cf26682f625997cf5506762ea89e68219e94a5
Inbar Pe'er <inbaronet@gmail.com>
2018-02-08 11:38:34 -0500

    :)

--- draw.c
+++ draw.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int x = x0;
+  int y = y0;
+  
+  d = 2A+B
+    while( x <= x1){
+    plot(x,y);
+    if(d>0){
+       y++;
+        d+=2B	
+    }
+    x++;
+    d+=2A
+}
+
 }
